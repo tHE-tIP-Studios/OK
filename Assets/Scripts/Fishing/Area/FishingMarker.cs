32f78909fe72216390e69fada34612b59f8839ac
Thomas the Franque <tfranque@gmail.com>
2020-05-01 19:24:24 +0100

    Add fishing area utility (BUTONSSSS)

---
+++ Assets/Scripts/Fishing/Area/FishingMarker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fishing.Area
+{
+    public class FishingMarker : MonoBehaviour
+    {
+        [SerializeField] private FishingArea _containingArea;
+
+        public FishingArea ContainingArea => _containingArea;
+
+        public void SetContainingArea(FishingArea area)
+        {
+            _containingArea = area;
+        }
+
+        public void Delete()
+        {
+            _containingArea.DeleteMarker(this);
+        }
+    }
+}
